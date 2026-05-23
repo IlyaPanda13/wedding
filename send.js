d1edd82f4b5703b2bc21bba2e2c2c77774ef6f87
IlyaPanda13 <samoilov777ilya@gmail.com>
2026-05-23 16:16:53 +0300

    проверка сайта

--- send.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('rsvpForm').addEventListener('submit', function() {
-    // Небольшая задержка, чтобы форма успела отправиться
-    setTimeout(function() {
-        document.getElementById('rsvpForm').style.display = 'none';
-        document.getElementById('rsvpSuccess').style.display = 'block';
-    }, 1000);
-});
